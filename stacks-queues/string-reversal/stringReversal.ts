c59419358b34954e112fdf1a3295a97c41ba9427
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 12:53:40 -0700

    Implemented stringReversal
    - tests passing

--- stacks-queues/string-reversal/stringReversal.ts
+++ stacks-queues/string-reversal/stringReversal.ts
@@ -3,7 +3,14 @@ import { StackStr } from "../common/stack";
 /** Return reverse of string. */
 
 function stringReversal(str: string): string {
-  return "x";
+  const stack = new StackStr([...str]);
+  let reversedString = "";
+
+  while (!stack.isEmpty()) {
+    reversedString += stack.pop();
+  }
+
+  return reversedString;
 }
 
 export { stringReversal };
