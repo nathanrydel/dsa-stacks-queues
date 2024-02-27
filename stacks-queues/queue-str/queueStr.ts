a8ae6bc6f394d26aa7a3eeb42339f197bf8662fc
Nathan Rydel <nathanrydel@gmail.com>
2024-02-26 20:11:29 -0700

    Implemented class methods
    - Tests passing

--- stacks-queues/queue-str/queueStr.ts
+++ stacks-queues/queue-str/queueStr.ts
@@ -1,28 +1,34 @@
+import { IndexError, LLStr } from "../common/ll";
+
 /** QueueStr: can remove from start or add to end */
 
 class QueueStr {
+  private _ll: LLStr;
 
   constructor(initial: string[] = []) {
+    this._ll = new LLStr(initial);
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    return this._ll.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    if (this._ll.length === 0) throw new IndexError();
+    return this._ll.shift();
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    return this._ll.head!.val;
   }
 
   /** peek(): return the value of top. */
   isEmpty(): boolean {
-    return false;
+    return this._ll.length === 0;
   }
 }
 
