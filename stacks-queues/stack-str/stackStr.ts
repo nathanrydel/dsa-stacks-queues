6b1c9bed928b4fbd6439b455b2096f0177144aaf
Nathan Rydel <nathanrydel@gmail.com>
2024-02-26 19:23:48 -0700

    tests passing

--- stacks-queues/stack-str/stackStr.ts
+++ stacks-queues/stack-str/stackStr.ts
@@ -3,30 +3,34 @@ import { IndexError } from "../common/ll";
 /** StackStr: can remove from the top or add to the top. */
 
 class StackStr {
+  private _lst: string[];
 
   constructor(initial: string[] = []) {
+    this._lst = initial;
   }
 
   /** push(val): add val to top. Returns undefined. */
   push(val: string): void {
+    this._lst.push(val);
   }
 
   /** pop(): remove and return item from top.
    * Throws error if empty. */
   pop(): string {
-    return "x";
+    if (this._lst.length === 0) throw new IndexError();
+    return this._lst.pop()!;
   }
 
   /** peek(): return the value of first item. */
-  peek():string {
-    return "x";
+  peek(): string {
+    if (this._lst.length === 0) throw new IndexError();
+    return this._lst[this._lst.length - 1];
   }
 
-  /** peek(): return the value of top. */
-  isEmpty():boolean {
-    return false;
+  /** isEmpty(): return bool check if stack has length */
+  isEmpty(): boolean {
+    return this._lst.length === 0;
   }
 }
-
 
 export { StackStr };
