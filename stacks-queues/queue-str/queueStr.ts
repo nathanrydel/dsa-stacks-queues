91a91fcfb5732a5472ccdf4cc4ec42796cb0c0e3
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 12:48:35 -0700

    Implemented queueStr
    - used common LLStr class as base for linked list

--- stacks-queues/queue-str/queueStr.ts
+++ stacks-queues/queue-str/queueStr.ts
@@ -1,30 +1,34 @@
+import { LLStr } from "../common/ll";
+
 /** QueueStr: can remove from start or add to end */
 
 class QueueStr {
+  private _ll: LLStr;
 
   constructor(initial: string[] = []) {
+    this._ll = new LLStr(initial);
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    return this._ll.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    return this._ll.shift();
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    return this._ll.head!.val;
   }
 
-  /** peek(): return the value of top. */
+  /** isEmpty(): return bool check of if _ll is empty */
   isEmpty(): boolean {
-    return false;
+    return this._ll.length === 0;
   }
 }
-
 
 export { QueueStr };
